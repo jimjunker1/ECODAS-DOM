3dd30871916baad6ea9f99219ea3f887e7ee73b0
jimjunker1 <james.junker1@gmail.com>
2020-08-11 09:19:33 -0500

    updated Soares data and data import

--- datascript.R
+++ datascript.R
@@ -3,6 +3,6 @@ source("install-packages.R")
 # Catalan et al. 2016 #
 
 csvs <- list.files(path = "./data/raw/", "*.csv", full.names = TRUE)
-catalan_2016 <- lapply(csvs, read_csv) %>% data.frame %>% rename(wrt_yrs = "Water.Retention.Time..years.",
+data <- lapply(csvs, read_csv) %>% data.frame %>% rename(wrt_yrs = "Water.Retention.Time..years.",
                                                                  degrate_perday = "Degradation.rate.temperature.corrected..d.1.") %>%
   select(-X12) %>% filter(degrate_perday > 0)
